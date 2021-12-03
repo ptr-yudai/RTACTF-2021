e38ba5496c2629154c07ad26a1f3439c3de99248
Yudai Fujiwara <ptr.yudai+dev@gmail.com>
2021-12-03 21:13:48 +0900

    Add leaky RSA

---
+++ crypto/leaky_rsa/distfiles/chall.py
@@ -0,0 +1,22 @@
+from Crypto.Util.number import getPrime, isPrime, inverse
+import os
+
+if __name__ == '__main__':
+    # Plaintext (FLAG)
+    m = int.from_bytes(os.getenv("FLAG", "FAKE{sample_flag}").encode(), 'big')
+
+    # Generate key
+    p = getPrime(600)
+    q = getPrime(500)
+    n = p*p*q*q
+    e = 65537
+    s = ((p**3 - 20211218*q) * inverse(p*p+q*q,n)) % n # tekitou (ha?)
+
+    # Encryption
+    c = pow(m, e, n)
+
+    # Information disclosure
+    print(f"n = 0x{n:x}")
+    print(f"e = 0x{e:x}")
+    print(f"c = 0x{c:x}")
+    print(f"s = 0x{s:x}")
---
+++ crypto/leaky_rsa/solution/solve.sage
@@ -0,0 +1,19 @@
+with open("../distfiles/output.txt", "r") as f:
+    exec(f.read())
+
+P.<x> = PolynomialRing(Zmod(n))
+f = (s*x + 20211218).monic()
+
+kbits = 512
+sol = f.small_roots(X=1<<kbits, beta=0.3)
+q = int(sol[0])
+
+pq = int(sqrt(int(n)))
+assert pq % q == 0
+p = pq // q
+
+phi = p*q*(p-1)*(q-1)
+d = inverse_mod(e, phi)
+m = pow(c, d, n)
+
+print(int.to_bytes(int(m), 256, 'big').lstrip(b'\0'))
